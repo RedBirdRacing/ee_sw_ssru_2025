02884598e20bf92ea695043a487e1ea3c383464a
Jasmine <leehochingjasmine@gmail.com>
2025-07-19 16:58:13 +0800

    separate can ids for front and rear SSRUs

--- include/Enums.h
+++ include/Enums.h
@@ -1,22 +1,36 @@
 #ifndef ENUMS_H
-#define ENUMS_H 
+#define ENUMS_H
+
 #include <Arduino.h>
 
 namespace SSRU_CAN {
     // CAN ID Definitions
-    constexpr uint16_t TORQUE_CMD_ID = 0x201;
-    constexpr uint16_t SENSOR_BASE_ID = 0x700;
-    
-    // Individual sensor IDs
-    enum SensorID : uint16_t {
-        BRAKE_TEMP1 = SENSOR_BASE_ID,
-        BRAKE_TEMP2,
+    constexpr uint16_t TORQUE_CMD_ID = 0x201; // Torque command from VCU
+    constexpr uint16_t ERROR_MSG_ID = 0x202; // Error message ID
+
+    // Front SSRU Sensor IDs (starting from 0x700)
+    enum Front_SSRU_SensorID : uint16_t {
+        FRONT_POT1 = 0x700,
+        FRONT_POT2,
+        FRONT_BRAKE_TEMP1,
+        FRONT_BRAKE_TEMP2,
         LVB_VOLTAGE,
         PUMP_TEMP,
         ENCODER_COUNT,
         PUMP_FLOW,
         TEMP_SENSOR,
-        LVB_TEMP
+        LVB_TEMP,
+        FRONT_MPU6050_1,
+        FRONT_MPU6050_2
+    };
+
+    // Rear SSRU Sensor IDs (starting from 0x750)
+    enum Rear_SSRU_SensorID : uint16_t {
+        REAR_POT1 = 0x750,
+        REAR_POT2,
+        REAR_BRAKE1,
+        REAR_BRAKE2,
+        REAR_MPU6050_3
     };
 }
 
