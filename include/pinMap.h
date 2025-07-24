822fd3756715d6cf0d52045b8d88a8798fef2cf8
Jasmine <leehochingjasmine@gmail.com>
2025-07-24 22:39:42 +0800

    updated pinMap.h for SSRU_v3.1

--- include/pinMap.h
+++ include/pinMap.h
@@ -2,40 +2,34 @@
 #define PINMAP_H
 
 // === ADC pins ===
-#define POT1_RAW ADC7
-#define POT2_RAW ADC6
+#define POT1_IN ADC7
+#define POT2_IN ADC6
 
-#define BRAKE_TEMP1_RAW PC0
-#define BRAKE_TEMP2_RAW PC1
-
-#define LVB_VOLTAGE_RAW PC2 //LVB voltage
-#define PUMP_TEMP_RAW PC3
-
-// === Digital pins ===
-#define ENCODER_A PD3
-#define ENCODER_B PD4
-
-#define PUMP_FLOW_RAW  PD5
-#define TEMP_SENSOR_RAW PD6
-#define LVB_TEMP_RAW PD7
+#define BRAKE_TEMP1_IN PC0
+#define BRAKE_TEMP2_IN PC1
 
 // === I2C Pins ===
 #define SDA PC4
 #define SCL PC5
-
-// MPU6050 Gyro/Accel addresses
 #define MPU6050_ADDR_1 0x68
 #define MPU6050_ADDR_2 0x69
 
-// === CAN Bus Pins ===
-#define CS_CAN_DL PB0
-#define CS_CAN_SSRU PB1
+#define DS18B20_IN PD6
 
-// === LoRa Pins ===  
-#define LORA_CS PB2
-#define LORA_DIO0 PD2
-#define LORA_MISO PB4
-#define LORA_MOSI PB3
-#define LORA_SCK PB5
+#ifdef SSRU_FRONT
+    #define CS_CANF_1 PB0
+    #define CS_CANF_2 PB1
+
+    #define ENCODER_A PD2
+    #define ENCODER_B PD3
+
+#elif defined(SSRU_REAR)
+    #define CS_CANR_1 PB0
+    #define CS_CANR_2 PB1
+
+    #define PUMP_FLOW_IN PD4
+    #define PUMP_NTC_50K_IN PC3
+    #define PUMP_PWM_OUT PD5
+#endif
 
 #endif // PINMAP_H
